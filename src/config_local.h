bbd3b5a480a8b0a5820c46aba32c3a1ae791fcbc
Agencia As de Oro <54107096+chivoso@users.noreply.github.com>
2022-04-21 16:57:59 -0300

    ok

--- src/config_local.h
+++ src/config_local.h
@@ -1,8 +1,8 @@
-#include <arduino.h>
+
 //////////////////////////////////////////////////
 // Configuraciones Para solo este cartel
 //////////////////////////////////////////////////
 
-String AGENCIA= "GANADORA";           // quien es el actuañizador: CASA, GANADORA, 5ESQUINAS, PILARES, COLORADA, CHAPULIN
+String AGENCIA= "CASA";           // quien es el actualizador: CASA, GANADORA, 5ESQUINAS, PILARES, COLORADA, CHAPULIN
 
 
